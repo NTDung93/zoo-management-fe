f686fad15a2f0488b7be7f77b435e22f6b9efe6c
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-09-26 16:43:04 +0700

    Add map when fetch data to AnimalGallery

--- src/components/Animals/AnimalGallery.tsx
+++ src/components/Animals/AnimalGallery.tsx
@@ -1,217 +1,58 @@
+import { FC } from 'react';
 
-const AnimalGallery = () => {
-  return (
-    <div className="section--sm section--top">
-  <div className="section__head">
-    <div className="container">
-      <div className="row g-4 justify-content-center">
-        <div className="col-md-8 col-xl-6">
-          <div className="text-center">
-            <span className="section__particles">
-              <span className="section__subtitle section__subtitle-dash">
-                Animals
-              </span>
-            </span>
-            <h2 className="section__title mt-2">
-              Our Zoo <span>Animals</span>
-            </h2>
-            <p className="section__para mx-auto">
-              Phasellus consectetuer vestibulum elit. Ametusbibendum mattis non.
-              Vestibulum fringilla pede sit ameugue.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="container">
-    <div className="row g-4">
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-1.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Lion
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              African Plains{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-2.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Poison Green Frog
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              World of Reptiles{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-3.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Siberian Tiger
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              Tiger Mountain{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-4.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Hippos
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center"> Zoo Center </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-5.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Green Pythons
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              World of Reptiles{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-6.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Asian Elephant
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              World of Reptiles{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-7.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Blue Iguana
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              World of Reptiles{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4 col-xl-3">
-        <div className="animal-card">
-          <img
-            src="assets/images/img-8.png"
-            alt="image"
-            className="animal-card__img"
-          />
-          <div className="animal-card__body">
-            <h5 className="mt-0 mb-2 text-center">
-              <a
-                href="animal-details.html"
-                className="t-link t-link--base heading-clr d-inline-block"
-              >
-                Red Macaw
-              </a>
-            </h5>
-            <span className="d-block sm-text text-center">
-              {" "}
-              World of Reptiles{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-  )
+interface Animal {
+	animal: string;
+	image: string;
+	region: string;
 }
 
-export default AnimalGallery
+interface AnimalProps {
+	animals: Animal[];
+}
+
+const AnimalGallery: FC<AnimalProps> = ({ animals }) => {
+	return (
+		<div className='section--sm section--top'>
+			<div className='section__head'>
+				<div className='container'>
+					<div className='row g-4 justify-content-center'>
+						<div className='col-md-8 col-xl-6'>
+							<div className='text-center'>
+								<span className='section__particles'>
+									<span className='section__subtitle section__subtitle-dash'>Animals</span>
+								</span>
+								<h2 className='section__title mt-2'>
+									Our Zoo <span>Animals</span>
+								</h2>
+								<p className='section__para mx-auto'>
+									Phasellus consectetuer vestibulum elit. Ametusbibendum mattis non. Vestibulum fringilla pede sit ameugue.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='row g-4'>
+					{animals.map((animalData, index) => (
+						<div className='col-md-6 col-lg-4 col-xl-3' key={index}>
+							<div className='animal-card'>
+								<img src={animalData.image} alt='image' className='animal-card__img' />
+								<div className='animal-card__body'>
+									<h5 className='mt-0 mb-2 text-center'>
+										<a href={`animal-details.html?animal=${animalData.animal}`} className='t-link t-link--base heading-clr d-inline-block'>
+											{animalData.animal}
+										</a>
+									</h5>
+									<span className='d-block sm-text text-center'>{animalData.region}</span>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default AnimalGallery;
--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -3,15 +3,15 @@ import AnimalGallery from '../components/Animals/AnimalGallery';
 import ClientSectionSlider from '../components/Animals/ClientSectionSlider';
 import EventSection from '../components/Animals/EventSection';
 import MembershipSection from '../components/Animals/MembershipSection';
+import Banner from '../components/Banner/Banner';
 import FactSection from '../components/Home/FactSection';
 import FAQSection from '../components/Home/FaqSection';
-import Banner from '../components/Banner/Banner';
 
 const Animals: FC = () => {
 	return (
 		<>
 			<Banner pageName={'Animals'} singleName={'Animals'} />
-			<AnimalGallery />
+			<AnimalGallery animals={} />
 			<EventSection />
 			<FAQSection />
 			<FactSection />
